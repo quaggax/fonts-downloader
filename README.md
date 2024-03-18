b0acdf446111e3ad3346d994e2cb09ccc9d97408
quaggax <122413643+quaggax@users.noreply.github.com>
2024-03-19 00:56:04 +0100

    full Google Fonts Support

--- README.md
+++ README.md
@@ -1,36 +1,31 @@
 # Download Fonts from any Stylesheet
+
 explaination of the script
 
 This Script contacts external (web) servers. You are responsible for the servers that are contacted as a result of the use of this script.
 
 ## Usage
-This script was developed and tested on macOS.
-It requires the installation of `python` via Homebrew. This can be accomplished using the following command:
 
-```brew install python```
+This script was developed and tested on macOS.
+It requires the installation of `python` (at least 3.10) via Homebrew. This can be accomplished using the following command:
+
+```
+brew install python
+```
 
 Make sure to close and reopen your shell afterwards for all changes to take effect.
 
+Only Google Fonts is supported at the moment.
+
+Example:
 
 ```
-python3 download-fonts.py "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap"
+python3.11 download-fonts.py "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap"
 ```
 
-## To Do
-- [ ] Capture entire `@font-face` CSS block (to be able to extract CSS properties)
-- [ ] Extract `font-family` name to create sub-folder and rename files
-- [ ] Extract other CSS properties and rename files accordingly
-    - [ ] `font-style` - Use this value inside the file name.
-    - [ ] `font-weight` - Check if the weight value has an assigned name and use it for the file name. If the value doesn't have an assigned name use the number value as a name.
-    - [ ] `font-stretch`
-    - [ ] `font-display`
-    - [ ] `unicode-range` - Check if the ranges match against the predetermined Google Fonts ranges and assign range-name accordingly. If the ranges don't match, assign an index as a name and save the ranges for SCSS and CSS generation.
 ---
-- [ ] Generate SCSS (with all extracted CSS properties)
-- [ ] Generate CSS (with all extracted CSS properties)
 
+## To-Do
 
-
-
-### Notes
-Parts of this code were generated with the help of Generative AI.
+- [ ] add option to generate CSS instead of SCSS
+- [ ] add support for more stylesheets
--- download-fonts.py
+++ download-fonts.py
@@ -14,34 +14,148 @@ finally:
 import re
 import argparse
 
+WEIGHT_NAMES = {
+	"thin": 100,
+	"extra-light": 200,
+	"light": 300,
+	"regular": 400,
+	"medium": 500,
+	"semi-bold": 600,
+	"bold": 700,
+	"extra-bold": 800,
+	"heavy": 900,
+	"extra-black": 950,
+}
+
+
 # create an argument parser (add '--' before the name to make it optional)
 parser = argparse.ArgumentParser(description='Download Fonts files from a CSS file')
 parser.add_argument('input_url', metavar='input-url', type=str, help='the URL of the input CSS file')
+parser.add_argument('--prefix_path', metavar='prefix-path', type=str, help='path prefix of the font files URL (in the SCSS/CSS)')
 parser.add_argument('--dir_path', metavar='dir-path', type=str, help='path of the destination directory for the font files')
 
 # parse the command-line arguments
 args = parser.parse_args()
 
-# directory to save font files
-save_dir = "fonts/"
-if args.dir_path is not None:
-    save_dir = args.dir_path
+# URL prefix for font files
+if args.prefix_path is None:
+    prefix_path = "/assets/fonts"
+else:
+    prefix_path = args.prefix_path
 
-# make sure the directory exists
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+# directory to save font files
+if args.dir_path is None:
+    save_base = "fonts/"
+else:
+    save_base = args.dir_path
 
 # download the CSS file
 css_file = requests.get(args.input_url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}).text
 
-# extract all links to WOFF or WOFF2 file types from the stylesheet
-font_links = re.findall('url\((.*?)\)', css_file)
-print(font_links)
+# capture all @font-face declaration blocks
+# font_face_declarations = re.findall('@font-face\s*{.*?}', css_file, re.DOTALL)
+font_face_declarations = re.findall('/\*\s*(?P<UNICODE_NAME>[^\s]+)\s*\*/(?P<CSS_LINES>[^}]*)', css_file, re.S)
 
-# download all font files
-for link in font_links:
-    # if ".woff2" in link or ".woff" in link:
-        filename = link.split("/")[-1]
-        save_path = os.path.join(save_dir, filename)
-        font_file = requests.get(link, allow_redirects=True).content
-        open(save_path, "wb").write(font_file)
+# create array with font-face objects
+fonts = []
+for font_face_declaration in font_face_declarations:
+    unicode_name = font_face_declaration[0]
+    val = font_face_declaration[1]
+
+    font_family = re.findall('font-family: [\"\']([^\"\']+)', val)[0]
+    font_style = re.findall('font-style: ([^;]+)', val)[0]
+    font_weight = re.findall('font-weight: ([^;]+)', val)[0]
+    font_stretch = re.findall('font-stretch: ([^;]+)', val)[0]
+    font_display = re.findall('font-display: ([^;]+)', val)[0]
+    font_src = re.findall('url\((.*?)\)', val)[0]
+    font_unicode_range = re.findall('unicode-range: ([^;]*)', val)[0]
+
+    fonts.append(
+        {
+            "font-family": font_family,
+            "font-style": font_style,
+            "font-weight": font_weight,
+            "font-stretch": font_stretch,
+            "font-display": font_display,
+            "src": font_src,
+            "unicode-range": font_unicode_range,
+            "unicode-name": unicode_name
+        }
+    )
+
+# download fonts and generate SCSS
+scss_fonts = ""
+font_family_websafe = ""
+save_dir = ""
+
+for i, item in enumerate(fonts):
+    css_file_format = ""
+
+    font_family_websafe = item["font-family"].replace(" ", "-").lower()
+    filetype = item["src"].split(".")[-1]
+    match filetype:
+        case "woff2":
+            css_file_format = "woff2"
+        case "woff":
+            css_file_format = "woff"
+        case "ttf":
+            css_file_format = "truetype"
+        case "otf":
+            css_file_format = "opentype"
+    filename = font_family_websafe + "-" + item["font-weight"] + "-" + item["font-style"] + "-" + item["unicode-name"] + "." + filetype
+
+    # make sure the save directory exists
+    save_dir = save_base + font_family_websafe
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    # download and save font file
+    save_path = os.path.join(save_dir, filename)
+    font_file = requests.get(item["src"], allow_redirects=True).content
+    open(save_path, "wb").write(font_file)
+    print("Downloading " + filename)
+
+    # add scss map to list of fonts
+    scss_fonts += f"""(
+        "font-family": "{item['font-family']}",
+        "font-style": "{item['font-style']}",
+        "font-weight": "{item['font-weight']}",
+        "font-stretch": "{item['font-stretch']}",
+        "font-display": "{item['font-display']}",
+        "font-path": "{prefix_path + '/' + font_family_websafe + '/' + filename}",
+        "font-format": "{css_file_format}",
+        "unicode-range": "{item['unicode-range']}",
+        "unicode-name": "{item['unicode-name']}"
+    )"""
+
+    # add scss list divider
+    if i+1 < len(fonts):
+        scss_fonts += ", "
+
+# scss_fonts = re.sub(r"\n", "", scss_fonts) # remove newlines
+# scss_fonts = re.sub(r'\s+', ' ', scss_fonts) # remove consecutive spaces
+
+scss_file = f"""@use "sass:map";
+
+$fonts: [{scss_fonts}];
+"""
+
+scss_file += """
+@each $item in $fonts {
+    /* #{map.get($item, "unicode-name")} */
+    @font-face {
+       	font-family: map.get($item, "font-family");
+       	font-style: map.get($item, "font-style");
+       	font-weight: map.get($item, "font-weight");
+       	font-display: map.get($item, "font-display");
+       	src: url("#{map.get($item, 'font-path')}") format(map.get($item, "font-format"));
+       	unicode-range: map.get($item, "unicode-range");
+    }
+}
+"""
+
+# save SCSS file
+scss_path = save_base + font_family_websafe + ".scss"
+open(scss_path, "w").write(scss_file)
+print("Saving generated SCSS file")
+
+print("Successfully downloaded fonts to " + save_dir + "/ and saved SCSS as " + scss_path)
